d678b4e0290f446b075395026b7759bcecfb1401
Narayana <narayana.alfa@gmail.com>
2019-03-15 12:15:58 +0530

    d

---
+++ src/main/java/com/snp/app/DataAppApplication.java
@@ -0,0 +1,13 @@
+package com.snp.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataAppApplication.class, args);
+	}
+
+}
